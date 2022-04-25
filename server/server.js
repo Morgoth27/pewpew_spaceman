12ebf7626c9d1efdc776ba187ce14377e943068c
Ethan Buchanan <ethan@buchananville.com>
2022-04-25 11:17:04 -0700

    moved route in server and changed specific '/' path to accept all paths

--- server/server.js
+++ server/server.js
@@ -22,9 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -32,6 +30,10 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
   
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
